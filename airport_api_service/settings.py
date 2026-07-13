d5a4d1421c50ebcc2ef8d001290fa06acd87b55c
Vitaliy Stepanyuk <vist98@gmail.com>
2026-07-13 21:48:54 +0300

    feat: edit admin.py and settings.py

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5giwuut1(%wodie2^6pf2#c@&g9*zjpwfjg^(hb1_ffqfk_pc="
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost",]
 
